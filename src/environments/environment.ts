efede151800e0c3e69068e0870643eda559d5fbb
Sashaank Ramakrishnan <sashaankrk@gmail.com>
2021-07-14 19:30:59 +0530

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBU_suARdKHiy4eFah-eLdi9FmBhGHGyQE",
+    authDomain: "sashaankshop.firebaseapp.com",
+    projectId: "sashaankshop",
+    storageBucket: "sashaankshop.appspot.com",
+    messagingSenderId: "325264071372",
+    appId: "1:325264071372:web:1869e1c9b5c9ebb06a8069",
+    measurementId: "G-M7NFY8DJEG"
+  }
 };
 
 /*
