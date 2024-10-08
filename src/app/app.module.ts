ccff1c9b47e54136f14759f96fa8b8a21be34183
Sashaank Ramakrishnan <sashaankrk@gmail.com>
2024-10-08 13:09:18 +0530

    optimize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,23 +12,13 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { AdminAuthGuardService } from 'src/app/admin/services/admin-auth-guard.service';
 import { environment } from 'src/environments/environment';
 import { AdminModule } from './admin/admin.module';
-import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
-import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
-import { ProductFormComponent } from './admin/components/product-form/product-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
-import { OrderSuccessComponent } from './shopping/components/order-success/order-success.component';
-import { ProductFilterComponent } from './shopping/components/products/product-filter/product-filter.component';
-import { ProductsComponent } from './shopping/components/products/products.component';
-import { AuthGuardService } from './shared/services/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingCartSummaryComponent } from './shopping/components/shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './shopping/components/products/products.component';
 import { ShoppingModule } from './shopping/shopping.module';
 
 @NgModule({
--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthGuardService } from '../shared/services/auth-guard.service';
+import { SharedModule } from '../shared/shared.module';
 import { CheckOutComponent } from './components/check-out/check-out.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
@@ -7,10 +11,6 @@ import { ProductFilterComponent } from './components/products/product-filter/pro
 import { ProductsComponent } from './components/products/products.component';
 import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { AuthGuardService } from '../shared/services/auth-guard.service';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
